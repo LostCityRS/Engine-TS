e520aa9e7c6cd2c086f32ebc078dbdacbf07baec
Mathias Dejerud Rådstam <68670981+FlenarnDev@users.noreply.github.com>
2026-01-26 11:09:39 +0100

    Update OpObjEDecoder.ts

--- src/network/game/client/codec/OpObjEDecoder.ts
+++ src/network/game/client/codec/OpObjEDecoder.ts
@@ -1,4 +1,4 @@
-import Packet from '#/io/Packet.js'
+import Packet from '#/io/Packet.js';
 import ClientGameMessageDecoder from '#/network/game/client/ClientGameMessageDecoder.js';
 import ClientGameProt from '#/network/game/client/ClientGameProt.js';
 import OpObjE from '#/network/game/client/model/OpObjE.js';
