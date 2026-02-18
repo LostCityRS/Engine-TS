316caf9b2e1bc88d99eaf514fe944d4c379d1873
Flenarn <flenarn@hotmail.com>
2026-02-18 21:10:39 +0100

    feat: Basic implementations for OpLocE & OpObjE.

--- src/network/game/client/handler/OpObjEHandler.ts
+++ src/network/game/client/handler/OpObjEHandler.ts
@@ -1,10 +1,25 @@
+import ObjType from '#/cache/config/ObjType.js';
 import { NetworkPlayer } from '#/engine/entity/NetworkPlayer.js';
 import ClientGameMessageHandler from '#/network/game/client/ClientGameMessageHandler.js';
 import OpObjE from '#/network/game/client/model/OpObjE.js';
+import MessageGame from '#/network/game/server/model/MessageGame.js';
+import Environment from '#/util/Environment.js';
 
 export default class OpObjEHandler extends ClientGameMessageHandler<OpObjE> {
-    handle(_message: OpObjE, _player: NetworkPlayer): boolean {
-        // TODO
+    handle(message: OpObjE, player: NetworkPlayer): boolean {
+        const obj = ObjType.get(message.obj);
+        
+        if (obj && obj.desc) {
+            player.write(new MessageGame(obj.desc));
+            return true;
+        }
+        
+        if (!Environment.NODE_PRODUCTION) {
+            player.write(new MessageGame(`Obj id ${message.obj} has no description.`));
+        }
+        
+        player.write(new MessageGame(`It's a ${obj ? obj.name : 'object'}.`));
         return true;
+            
     }
 }
