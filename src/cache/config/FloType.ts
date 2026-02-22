7d26c3b23c95a31a09e95d5bc378354db5a650b4
Flenarn <flenarn@hotmail.com>
2026-02-22 16:03:29 +0100

    feat: Update FloType for revision 530.

--- src/cache/config/FloType.ts
+++ src/cache/config/FloType.ts
@@ -69,22 +69,49 @@ export default class FloType extends ConfigType {
 
     // ----
 
-    rgb: number = 0;
-    texture: number = -1;
-    overlay: boolean = false;
+    colour: number = 0;
+    materials: number = -1;
     occlude: boolean = true;
+    averagecolour: number = -1;
+    materialscale: number = 512;
+    hardshadow: boolean = true;
+    priority: number = 8;
+    blend: boolean = false;
+    waterfogcolour: number = 1190717;
+    waterfogscale: number = 512;
+    code8: boolean = false; // Client-only opcode marker
 
     decode(code: number, dat: Packet): void {
         if (code === 1) {
-            this.rgb = dat.g3();
+            this.colour = dat.g3();
         } else if (code === 2) {
-            this.texture = dat.g1();
+            this.materials = dat.g1();
         } else if (code === 3) {
-            this.overlay = true;
+            this.materials = dat.g2();
+            if (this.materials === 65535) {
+                this.materials = -1;
+            }
         } else if (code === 5) {
             this.occlude = false;
         } else if (code === 6) {
             this.debugname = dat.gjstr();
+        } else if (code === 7) {
+            this.averagecolour = dat.g3();
+        } else if (code === 8) {
+            // Client-only code, we store it only to write the opcode later during packing.
+            this.code8 = true;
+        } else if (code === 9) {
+            this.materialscale = dat.g2();
+        } else if (code === 10) {
+            this.hardshadow = false;
+        } else if (code === 11) {
+            this.priority = dat.g1();
+        } else if (code === 12) {
+            this.blend = true;
+        } else if (code === 13) {
+            this.waterfogcolour = dat.g3();
+        } else if (code === 14) {
+            this.waterfogscale = dat.g1();
         } else {
             throw new Error(`Unrecognized flo config code: ${code}`);
         }
