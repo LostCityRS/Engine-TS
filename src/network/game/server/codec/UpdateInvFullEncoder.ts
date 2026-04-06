940170a42ca54435025d1ce369c19c7f7adbacd7
Pazaz <pazaz@itspazaz.com>
2026-04-06 00:07:40 -0400

    fix: UPDATE_INV_FULL should only send up to the last slot used

--- src/network/game/server/codec/UpdateInvFullEncoder.ts
+++ src/network/game/server/codec/UpdateInvFullEncoder.ts
@@ -11,12 +11,22 @@ export default class UpdateInvFullEncoder extends ServerGameMessageEncoder<Updat
         const { component, inv } = message;
 
         const comType = Component.get(component);
+        // prevent a client crash by capping to inv size || interface size
         const size = Math.min(inv.capacity, comType.width * comType.height);
 
-        // todo: size should be the index of the last non-empty slot
-        buf.p2(component);
-        buf.p1(size);
+        // only send up to the last slot in use (anything beyond is cleared by the client)
+        let max = 0;
         for (let slot = 0; slot < size; slot++) {
+            const obj = inv.get(slot);
+
+            if (obj) {
+                max = slot + 1;
+            }
+        }
+
+        buf.p2(component);
+        buf.p1(max);
+        for (let slot = 0; slot < max; slot++) {
             const obj = inv.get(slot);
 
             if (obj) {
